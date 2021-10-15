baff08ba05b87e997db8c53f9fd22504e79b000d
Adam Warner <me@adamwarner.co.uk>
2021-10-15 16:59:04 +0100

    Tweak to catalina-wrapper.sh to work with named instances (can provide port OR instance name, not both) Utilise new env var for using named instance Add notes to Dockerfile
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- Microsoft SQL Server/klaros/files/catalina-wrapper.sh
+++ Microsoft SQL Server/klaros/files/catalina-wrapper.sh
@@ -68,9 +68,12 @@ else
 fi
 
 (
-	echo "hibernate.dialect=org.hibernate.dialect.SQLServer2008Dialect"
 	echo "hibernate.connection.driver_class=com.microsoft.sqlserver.jdbc.SQLServerDriver"
-	echo "hibernate.connection.url=jdbc:sqlserver://${DATABASE_HOST}:1433;databaseName=${DATABASE_NAME}"
+	if [ -z "${DATABASE_INSTANCE}" ]; then
+		echo "hibernate.connection.url=jdbc:sqlserver://${DATABASE_HOST}:1433;databaseName=${DATABASE_NAME}"
+	else
+		echo "hibernate.connection.url=jdbc:sqlserver://${DATABASE_HOST};instanceName=${DATABASE_INSTANCE};databaseName=${DATABASE_NAME}"
+	fi
 	echo "hibernate.connection.username=${DATABASE_USER}"
 	echo "hibernate.connection.password=${DATABASE_PASSWORD}"
 ) >/data/klaros-home/hibernate.properties
